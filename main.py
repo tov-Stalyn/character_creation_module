fd4c9964b5f0a9da92d49c2ddd70c25990c0899b
Руслан Мамедов <tov-stalyn@yandex.ru>
2022-10-28 00:13:10 +0400

    add working version without classes

--- main.py
+++ main.py
@@ -81,3 +81,19 @@ def choice_char_class() -> str:
                                ' другую кнопку, чтобы выбрать другого '
                                'персонажа ').lower()
     return char_class
+
+
+def main():
+    print('Приветствую тебя, искатель приключений!')
+    print('Прежде чем начать игру...')
+    char_name: str = input('...назови себя: ')
+    print(f'Здравствуй, {char_name}! '
+          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+    print('Ты можешь выбрать один из трёх путей силы:')
+    print('Воитель, Маг, Лекарь')
+    char_class: str = choice_char_class()
+    print(start_training(char_name, char_class))
+
+
+
+main()
